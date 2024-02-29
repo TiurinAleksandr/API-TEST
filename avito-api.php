3c74b0b8eaa8d7da052fffcd6a9d27ad09184258
TiurinAleksandr <109982907+TiurinAleksandr@users.noreply.github.com>
2024-02-29 16:23:28 +0200

    Update avito-api.php

--- avito-api.php
+++ avito-api.php
@@ -1,3 +1,9 @@
 <?
-echo "hello";
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+$array = array('first'=>'1', 'second'=>2);
+
+http_response_code(200);
+echo json_encode($array);
 ?>
