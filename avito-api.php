437dd5eb111ef2b6dc3476fa2472ef00ad5e5440
TiurinAleksandr <109982907+TiurinAleksandr@users.noreply.github.com>
2024-02-29 18:13:08 +0200

    Update avito-api.php

--- avito-api.php
+++ avito-api.php
@@ -2,7 +2,7 @@
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 
-$array = array('first'=>'1', 'second'=>2);
+$array = array('first'=>$_GET['first'], 'second'=>$_GET['second']);
 
 http_response_code(200);
 echo json_encode($array);
